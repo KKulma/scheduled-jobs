b1829c98e6c2bee40b549cd7a7c4dddf1362633c
Kasia <katarzyna.kulma@gmail.com>
2021-02-16 23:56:47 +0000

    add daily summary

--- daily-update-db.R
+++ daily-update-db.R
@@ -19,30 +19,38 @@ con <- DBI::dbConnect(
 # db data check
 logger::log_info("checking if the data alredy exists in the db")
 
-res <-
-  DBI::dbGetQuery(con,
-             "select MAX([to]) as max_to FROM national_ci_data")
-# 
-# all_dates <-
-#   DBI::dbGetQuery(con,
-#              "select DISTINCT [to] as unique_to FROM national_ci_data")
+
+transform_ci <- function(raw) {
+  raw %>%
+    dplyr::mutate(from_dt = lubridate::as_date(from),
+                  is_renewable = dplyr::if_else(fuel %in% c("nuclear", "hydro", "solar", "wind"), 1, 0)) %>%
+    dplyr::filter(is_renewable == 1) %>%
+    dplyr::group_by(from, from_dt, regionid, dnoregion, shortname) %>%
+    dplyr::summarise(total_perc = sum(perc)) %>% # calculate renewable perc by 1/2 hour interval
+    dplyr::group_by(from_dt, regionid, dnoregion, shortname) %>%
+    dplyr::summarise(renewable_perc = mean(total_perc)) %>%
+    dplyr::ungroup()
+}
 
 #national CI
 start <- lubridate::today() - lubridate::days(1)
 end <- start
 
-if (!all(lubridate::as_date(res$max_to) == start)) {
-  logger::log_info("pull data from the API")
-  
-  intense_data <- intensegRid::get_national_ci(start = start, end = end)
-  
-  logger::log_info("write new data to the db")
-  if (!is.null(intense_data)) {
-    DBI::dbWriteTable(con, "national_ci_data", intense_data, append = TRUE)
-  }
-} else {
-  logger::log_info("data already exists on DB")
+
+logger::log_info("pull data from the API")
+
+intense_data <-
+  intensegRid::get_national_ci(start = start, end = end)
+
+logger::log_info("write new data to the db")
+if (!is.null(intense_data)) {
+  # export 1/2-hourly data
+  DBI::dbWriteTable(con, "national_ci_data", intense_data, append = TRUE)
+  # export daily summary
+  new_entries = transform_ci(intense_data)
+  DBI::dbWriteTable(con, "daily_ci_data", new_entries, append = TRUE)
 }
+
 
 logger::log_info("disconnect from the db")
 DBI::dbDisconnect(con)
