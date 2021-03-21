dc19801a7d112d76d800d996532acd17161f1fc3
Kasia <katarzyna.kulma@gmail.com>
2021-03-21 22:20:52 +0000

    update SQL tbl name

--- daily-update-db.R
+++ daily-update-db.R
@@ -16,22 +16,6 @@ con <- DBI::dbConnect(
   Port      = 1433
 )
 
-# db data check
-logger::log_info("checking if the data alredy exists in the db")
-
-
-transform_ci <- function(raw) {
-  raw %>%
-    dplyr::mutate(from_dt = lubridate::as_date(from),
-                  is_renewable = dplyr::if_else(fuel %in% c("nuclear", "hydro", "solar", "wind"), 1, 0)) %>%
-    dplyr::filter(is_renewable == 1) %>%
-    dplyr::group_by(from, from_dt, regionid, dnoregion, shortname) %>%
-    dplyr::summarise(total_perc = sum(perc)) %>% # calculate renewable perc by 1/2 hour interval
-    dplyr::group_by(from_dt, regionid, dnoregion, shortname) %>%
-    dplyr::summarise(renewable_perc = mean(total_perc)) %>%
-    dplyr::ungroup()
-}
-
 #national CI
 start <- lubridate::today() - lubridate::days(1)
 end <- start
@@ -45,10 +29,7 @@ intense_data <-
 logger::log_info("write new data to the db")
 if (!is.null(intense_data)) {
   # export 1/2-hourly data
-  DBI::dbWriteTable(con, "national_ci_data", intense_data, append = TRUE)
-  # export daily summary
-  new_entries = transform_ci(intense_data)
-  DBI::dbWriteTable(con, "daily_ci_data", new_entries, append = TRUE)
+  DBI::dbWriteTable(con, "raw_ci_by_country", intense_data, append = TRUE)
 }
 
 
